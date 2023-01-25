4d55a385689c3b1b3640aa27544aa707feb527c7
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-25 23:17:23 +0100

    add AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script>
 import axios from 'axios';
 import { store } from './store/store.js';
+import AppFooter from './components/AppFooter.vue'
+
 export default {
   name: 'Breaking Bad',
   data() {
     return {
       store
     }
-  }
+  },
+  components: { AppFooter }
 }
 </script>
 
 <template>
   <main>
-
+    <app-footer></app-footer>
   </main>
 </template>
 
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from '../store/store';
+export default {
+  name: 'AppFooter',
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+
+<template>
+  <footer class="d-flex justify-content-center flex-align-center">
+    <img class="img-fluid" src="../assets/pokemon-logo.png" alt="Logo Pokemon">
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  height: 150px;
+}
+</style>
